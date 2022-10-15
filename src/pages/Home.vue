888102382ae0b4f647a1d5fbae2b180a5f14b968
agusoler000 <agustin.soler@mcassociats.com>
2022-10-15 12:06:58 +0200

    feature: components

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,45 +1,40 @@
 <script>
-import TodoApp from "../components/TodoApp.vue";
-import Exercices from "../components/Exercices.vue";
-import Forms from "../components/forms.vue";
-import Searcher from "../components/searcher.vue";
+import TodoApp from '../components/TodoApp.vue';
+import Searcher from '../components/searcher.vue';
+
 export default {
   data() {
     return {
-        wordToSearch: ""
-    }
+      wordToSearch: '',
+    };
   },
   components: {
     TodoApp,
-    Exercices,
-    Forms,
     Searcher,
   },
   methods: {
-    onSearchByWord (value) {
-        console.log("valor emitido desde componente hijo:", value)
-        this.wordToSearch = value
-    }
+    onSearchByWord(value) {
+      console.log('valor emitido desde componente hijo:', value);
+      this.wordToSearch = value;
+    },
   },
-    
-  
 };
 </script>
 <template>
   <div class="container py-5">
     <header>
       <h2 class="text-center mb-5" id="header">
-      Vue TODO APP- GROUP 3
-      <font-awesome-icon icon="fa-solid fa-user-secret" />
-    </h2>
+        Vue TODO APP- GROUP 3
+        <font-awesome-icon icon="fa-solid fa-user-secret" />
+      </h2>
     </header>
-   
+
     <section class="container mb-1">
       <div class="row justify-content-between">
         <div class="col-4">Filter</div>
-        <Searcher @emitInput="onSearchByWord($event)"/>
+        <Searcher @emitInput="onSearchByWord($event)" />
       </div>
     </section>
-    <TodoApp :wordToSearch="wordToSearch"/>
+    <TodoApp :wordToSearch="wordToSearch" />
   </div>
 </template>
