e6f3582487ab255856e369471c6a959258772c5d
Nuria Viana Pérez <nuria.viana@students.salle.url.edu>
2022-10-22 10:42:06 +0200

    router in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,8 @@
         rounded
         class="ml-2"
       >
-        {{ link }}
+        <router-link v-if="link.isRouterLink" :to="link.href"> {{link.label}} </router-link>
+        <a v-else :href="link.href" target="_blank"> {{link.label}} </a>
       </v-btn>
       <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} — <strong>ToDo App- Grupo 3</strong>
@@ -20,8 +21,15 @@
 <script>
 export default {
   data: () => ({
-    links: ['Home', 'About Us', 'GitHub', 'Linkedin', 'Contact Us'],
-    dialog: false,
+    links: [
+      { label: "Home", isRouterLink: true, href: "/" },
+      { label: "About Us", isRouterLink: true, href: "/about-us" },
+      {
+        label: "GitHub",
+        isRouterLink: false,
+        href: "https://github.com/nvianaperez/ToDoList-group3",
+      },
+    ]    
   }),
 };
 </script>
